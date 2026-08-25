173dc5490d2531a4c8dab47ad6418a8f4346f55b
Richard Gieg <richgieg@users.noreply.github.com>
2026-08-25 10:51:54 -0700

    Update lib and use getClientStateAndClearEventsFactory

--- src/game/index.ts
+++ src/game/index.ts
@@ -2,11 +2,11 @@ export { MAX_PLAYERS } from "./constants.js";
 
 export { createGame } from "./actions/createGame.js";
 export { getClientStateAndClearEvents } from "./actions/getClientStateAndClearEvents.js";
+export { getEventsAndClearAcknowledged } from "./actions/getEventsAndClearAcknowledged.js";
 export { joinGame } from "./actions/joinGame.js";
 export { leaveGame } from "./actions/leaveGame.js";
+export { sendChat } from "./actions/sendChat.js";
 export { startGame } from "./actions/startGame.js";
-
-export { getEventsAndClearAcknowledged, sendChat } from "@hellacardgames/lib";
 
 export type { Card } from "./types/Card.js";
 export type { ChatMessage } from "./types/ChatMessage.js";

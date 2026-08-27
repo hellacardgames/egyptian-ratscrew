3d41757db3d25da687949ea77ffe6fbec801d538
Richard Gieg <richgieg@users.noreply.github.com>
2026-08-27 11:20:13 -0700

    Export types Manger, Server, Client

--- src/manager/createManager.ts
+++ src/manager/createManager.ts
@@ -10,6 +10,8 @@ import {
   startGame,
 } from "../game/index.js";
 
+export type Manager = ReturnType<typeof createManager>;
+
 export const createManager = createManagerFactory({
   maxPlayers: MAX_PLAYERS,
   createGame,
